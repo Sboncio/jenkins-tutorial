cdc5bde4ecaf48b6105b06660a56a90de2777ad8
Sam <samuelasquith@btinternet.com>
2020-08-20 12:05:00 +0000

    Second

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #FILE = "/chaperootodo_client/"
-if [ -d "chaperootodo_client" ] && \
+[ -d "chaperootodo_client" ] && \
     echo "chaperootodo_client already exists" || \
     git clone https://gitlab.com/qacdevops/chaperootodo_client.git
