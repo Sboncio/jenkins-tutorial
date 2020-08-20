fa3119ff225ddc5fe4d3bea61432ce22c43ae36e
Sam <samuelasquith@btinternet.com>
2020-08-20 13:22:56 +0000

    second

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-apt update
+sudo apt update
 curl https://get.docker.com | sudo bash
 sudo usermod -aG docker $(whoami)
+sudo apt update
 sudo apt install -y curl jq
 # set which version to download (latest)
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
