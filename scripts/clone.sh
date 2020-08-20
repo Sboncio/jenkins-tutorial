741f2b1adef6e3e02ca1e1d83df8a0481af0f483
Sam <samuelasquith@btinternet.com>
2020-08-20 11:56:34 +0000

    Second

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #FILE = /chaperootodo_client 
-if [ -d "./chaperootodo_client " ]; then
+if [ -d "./chaperootodo_client " ] then
     echo "$FILE already exists"
 else
     git clone https://gitlab.com/qacdevops/chaperootodo_client.git
